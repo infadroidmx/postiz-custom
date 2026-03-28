02fdca42965848cf4bc5f61161999c38f2320ad7
infadroidmx <felix@patriotetching.com>
2026-03-27 23:56:08 -0700

    Fix logo layout: resize to fit sidebar and stop overlapping menu

--- apps/frontend/src/components/new-layout/logo.tsx
+++ apps/frontend/src/components/new-layout/logo.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export const Logo = () => {
   return (
-    <img src="/logo.png" alt="Infinate Post" className="mt-[8px] min-w-[200px] max-h-[100px] object-contain" />
+    <img src="/logo.png" alt="Infinate Post" className="mt-[8px] w-[48px] object-contain mx-auto" />
   );
 };
